29f2343e71674fc30064179cf496d8726a6d79b2
Anna <annakatarina1994@gmail.com>
2022-07-25 19:21:44 -0400

    displaying user information in table

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const ALL_USERS_QUERY = gql`
 `;
 
 const App = () => {
-  const { loading, error, data } = useQuery(ALL_USERS_QUERY);
+  const { loading, error, data: userData } = useQuery(ALL_USERS_QUERY);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -28,7 +28,7 @@ const App = () => {
     <pre>
         <h1>Users</h1>
       {/* <code>{JSON.stringify(data, null, 2)}</code> */}
-      <UserTable userData={data} />
+      <UserTable userData={userData} />
     </pre>
   );
 };
--- src/component/UserTable.jsx
+++ src/component/UserTable.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-const UserTable = (userData) => {
-    return (
-        <div>
-            {JSON.stringify(userData, null, 2)}
-
-        </div>
-    )
-}
+const UserTable = ({ userData }) => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th></th>
+          <th>Email</th>
+          <th>Name</th>
+          <th>Role</th>
+        </tr>
+        {userData.allUsers.map((user, index) => {
+          return (
+            <tr key={index}>
+              <input type="checkbox" />
+              <td>{user.email}</td>
+              <td>{user.name}</td>
+              <td>{user.role}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
 
 export default UserTable;
