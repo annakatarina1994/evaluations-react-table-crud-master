9e75f0168c217fe0456530bd865c439e2649dd2d
Anna <annakatarina1994@gmail.com>
2022-07-30 20:51:45 -0400

    bunch of styling

--- src/component/UserDetails.jsx
+++ src/component/UserDetails.jsx
@@ -2,65 +2,98 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { useMutation } from '@apollo/react-hooks';
+import '../style/UserDetails.css';
 
+// confused about what I need back...user or email and attributes again...??
 const UPDATE_USER = gql`
-    mutation updateUser {
-        email,
-        newAttributes
+  mutation updateUser($email: ID!, $newAttributes: UserAttributesInput!) {
+    updateUser(email: $email, newAttributes: $newAttributes) {
+      User {
+        name
+      }
     }
-  `;
+  }
+`;
 
 const UserDetails = () => {
   const location = useLocation();
   const { name, email, role } = location.state;
   const [selected, setSelected] = useState(role);
-  const [updateUser, {data, loading, error}] = useMutation(UPDATE_USE);
+  const [updateUser, { data, loading, error }] = useMutation(UPDATE_USER, {});
+  let input;
+
+  if (loading) return 'Loading...';
+  if (error) return `Error Saving! ${error.message}`;
 
   // not completed yet, revisit
   function handleChange(event) {
     setSelected(event.target.value);
-    console.log("Selected?: ", selected);
+    console.log('Selected?: ', selected);
   }
 
   return (
-    <div>
-      <section>
-        <h1>{email}</h1>
-        <button>Save</button>
-      </section>
-      <section className="nameSection">
-        <label>Name</label>
-        <input type="text" name="name" placeholder={name} />
-      </section>
-      <section className="roleSection">
-        <p>Role</p>
-        <input type="radio" value="Admin" checked={selected === 'ADMIN'} onChange={handleChange} />
-        <label>Admin</label>
-        <input
-          type="radio"
-          value="Developer"
-          checked={selected === 'DEVELOPER'}
-          onChange={handleChange}
-        />
-        <label>Developer</label>
-        <input
-          type="radio"
-          value="App Manager"
-          checked={selected === 'APP_MANAGER'}
-          onChange={handleChange}
-        />
-        <label>App Manager</label>
-        <input
-          type="radio"
-          value="Marketing"
-          checked={selected === 'MARKETING'}
-          onChange={handleChange}
-        />
-        <label>Marketing</label>
-        <input type="radio" value="Sales" checked={selected === 'SALES'} onChange={handleChange} />
-        <label>Sales</label>
-      </section>
-    </div>
+      <form
+      className='userDetailsForm'
+        onSubmit={(e) => {
+          updateUser({ variables: { email: email, newAttributes: input.value } });
+          input.value = '';
+        }}
+      >
+        <section className='heading'>
+          <h1 id="email">{email}</h1>
+          {/* add an onsubmit to this button, where i think we'd want to call our mutation? maybe? */}
+          <button type="submit" id="saveButton">Save</button>
+        </section>
+        <section className="nameSection">
+          <label>Name</label>
+          <input
+            ref={(node) => {
+              input = node;
+            }}
+            type="text"
+            name="name"
+            placeholder={name}
+          />
+        </section>
+        <section className="roleSection">
+          <p>Role</p>
+          <input
+            type="radio"
+            value="Admin"
+            checked={selected === 'ADMIN'}
+            onChange={handleChange}
+          />
+          <label>Admin</label>
+          <input
+            type="radio"
+            value="Developer"
+            checked={selected === 'DEVELOPER'}
+            onChange={handleChange}
+          />
+          <label>Developer</label>
+          <input
+            type="radio"
+            value="App Manager"
+            checked={selected === 'APP_MANAGER'}
+            onChange={handleChange}
+          />
+          <label>App Manager</label>
+          <input
+            type="radio"
+            value="Marketing"
+            checked={selected === 'MARKETING'}
+            onChange={handleChange}
+          />
+          <label>Marketing</label>
+          <input
+            type="radio"
+            value="Sales"
+            checked={selected === 'SALES'}
+            onChange={handleChange}
+          />
+          <label>Sales</label>
+        </section>
+      </form>
   );
 };
 
--- src/component/UserTable.jsx
+++ src/component/UserTable.jsx
@@ -1,37 +1,66 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { gql } from 'apollo-boost';
 import '../style/UserTable.css';
 
+const DELETE_USER = gql`
+  mutation deleteUsers($emails: [ID]!) {
+    deleteUsers(emails: $emails) {
+      ID
+    }
+  }
+`;
+
 const UserTable = ({ userData }) => {
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  function capitalize(str) {
+    str = str
+      .toLowerCase()
+      .split('_')
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(' ');
+    return str;
+  }
+
+  function handleChange(e) {
+    if (e.target.checked === true) {
+      setSelectedUsers([...selectedUsers, Number(e.target.value)]);
+    } else {
+      const activeUsers = selectedUsers.filter((x) => {
+        if (x === Number(e.target.value)) return false;
+        return true;
+      });
+      setSelectedUsers([...activeUsers]);
+    }
+  }
 
   return (
     <>
-      <h1>Users</h1>
-      <button disabled>Delete</button>
-      <table className='userTable'>
-        <thead>
-          <tr className="tableHeader">
-            <th></th>
-            <th>Email</th>
-            <th>Name</th>
-            <th>Role</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userData.allUsers.map((user, index) => {
-            return (
-              <Link key={index} to={user.name} state={user}>
-                <tr key={index} className="tableRows">
-                  <input type="checkbox" />
-                  <td className="tableRows">{user.email}</td>
-                  <td className="tableRows">{user.name}</td>
-                  <td className="tableRows">{user.role}</td>
-                </tr>
-              </Link>
-            );
-          })}
-        </tbody>
-      </table>
+      <section className="tableHeadingSection">
+        <h1 className="tableTitle">Users</h1>
+        <button disabled className="deleteButton">
+          Delete
+        </button>
+      </section>
+      <section className="tableHeadersSection">
+        <p id="checkBoxColumn"></p>
+        <p id="emailColumn">Email</p>
+        <p id="nameColumn">Name</p>
+        <p id="roleColumn">Role</p>
+      </section>
+      {userData.allUsers.map((user, index) => {
+        return (
+          <div className="userRows">
+            <Link key={index} to={user.name} state={user} className="userRowLink">
+              <input id="checkBox" type="checkbox" onChange={handleChange} value={user.email} />
+              <p id="email">{user.email}</p>
+              <p id="name">{user.name}</p>
+              <p id="role">{capitalize(user.role)}</p>
+            </Link>
+          </div>
+        );
+      })}
     </>
   );
 };
